107781df31dadbf2b39d041a080ea820fa50b77e
Nick Pettazzoni <npettazz@akamai.com>
2012-12-01 13:25:42 -0500

    oops, ignore the actual celeryconfig and include the example

---
+++ celeryconfig-example.py
@@ -0,0 +1,9 @@
+CELERY_IMPORTS = ("tasks", )
+
+BROKER_URL = 'amqp://'
+CELERY_RESULT_BACKEND = 'amqp://'
+
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/New_York'
+CELERY_ENABLE_UTC = True
