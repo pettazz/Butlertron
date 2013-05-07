910ad0090ff134956407bc03a69fb87b3dfd1427
Nick Pettazzoni <npettazz@akamai.com>
2013-05-07 15:19:07 -0400

    update config example to be realistic

--- core/config-example.py
+++ core/config-example.py
@@ -2,20 +2,25 @@
 This contains all the main app settings
 """
 
+class BUTL:
+    API = {
+        'host_pattern': r'localhost',
+        'host_port': '8888'
+    }
 
+class TWILIO:
+    ACCOUNT = {
+        'sid': 'butts',
+        'auth_token': 'lmao',
+        'number': '+4135962244',
+        'split_long_sms': True
+    }
 
-TWILIO = {
-    'ACCOUNT_SID': 'butts',
-    'AUTH_TOKEN': 'lmao',
-    'phone_number': '+4135962244',
-    'split_long_sms': True
-}
-
-DATABASE = {
-    'type': 'mysql',
-    'server': 'db01.local',
-    'port': 3306,
-    'user': 'dbuser',
-    'password': 'dbpass', 
-    'schema': 'Hammoocks'
-}
+class DATABASE:
+    MYSQL = {
+        'server': 'osto.us',
+        'port': 3306,
+        'user': 'butl',
+        'password': 'dbpass', 
+        'schema': 'Hammoocks'
+    }
