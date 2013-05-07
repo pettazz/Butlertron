44b174232451e4f0db57fbb302b934c34e08a0ec
Nick Pettazzoni <npettazz@akamai.com>
2013-05-07 15:02:52 -0400

    get ready to figure out twilio cookies

--- event_handlers/sms_handler.py
+++ event_handlers/sms_handler.py
@@ -1,9 +1,12 @@
 from basehandler import BaseHandler
 
 class SmsEventHandler(BaseHandler):
+    BUTL_EVENT_API_PATTERNS = ['sms']
+
     def write_error(status_code, **kwargs):
         pass
 
     def post(self):
+        print self.cookies
         event_id = self.get_argument("event_id", default=None)
-        workflow_id = self.get_argument("workflow_id", default=None)
+        
